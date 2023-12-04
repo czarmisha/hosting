764c8a8d68446577e75e79d79f5ce15740726263
czar_code <vertigomisha@gmail.com>
2023-12-04 14:35:14 +0500

    migrations

--- main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from api.hosting.routes import hosting_app
-
-app = FastAPI()
-
-app.include_router(hosting_app, prefix='/hosting')
-
-
-# при отправки видео создавать таск в селери и сразу отвечать что файл загружается ждите
