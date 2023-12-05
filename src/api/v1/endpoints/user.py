edf636d89c430bd6f9b4e68516c6fbb058ee1bde
czar_code <vertigomisha@gmail.com>
2023-12-05 15:17:43 +0500

    use repositorie for db craud

--- src/api/v1/endpoints/user.py
+++ src/api/v1/endpoints/user.py
@@ -1,12 +1,8 @@
-from typing import Annotated
-
 from fastapi import APIRouter, Depends, Form, HTTPException, status
 from pydantic import ValidationError
-from sqlalchemy import insert
-from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.db.models.user import User as UserModel
-from src.deps.session import get_session
+from src.deps.repo import get_repo
+from src.repos.user import UserRepo
 from src.schemas.user import User, UserOut
 
 user_router = APIRouter()
@@ -14,8 +10,8 @@ user_router = APIRouter()
 
 @user_router.post("/create", status_code=status.HTTP_201_CREATED)
 async def create_user(
-    username: Annotated[str, Form()],
-    session: Annotated[AsyncSession, Depends(get_session)],
+    username: str = Form(),
+    user_repo: UserRepo = Depends(get_repo(UserRepo)),
 ) -> UserOut:
 
     try:
@@ -23,8 +19,5 @@ async def create_user(
     except ValidationError:
         raise HTTPException(status.HTTP_400_BAD_REQUEST, detail="Validation error")
 
-    stmt = insert(UserModel).returning(UserModel).values(**user.model_dump())
-    db_user = await session.scalar(stmt)
-    await session.commit()
-    user_out = UserOut.model_validate(db_user)
-    return user_out
+    user = await user_repo.create(user)
+    return user
